9950e5221d2250183b0d54c36db4fa8ad063d06f
Alexander Cyril <sanchez.alexander.cyril@com>
2025-03-10 10:02:14 +0100

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,4 @@
 
-// import { resolve } from "path";
-// import { defineConfig } from "vite";
-
-// export default defineConfig({
-//   root: "src/",
-//   build: {
-//     outDir: "../dist",
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, "src/index.html"),
-//         cart: resolve(__dirname, "src/cart/index.html"),
-//         checkout: resolve(__dirname, "src/checkout/index.html"),
-//         product: resolve(__dirname, "src/product_pages/index.html"),
-//       },
-//     },
-//   },
-// });
-
-
-
 
 // import { resolve } from "path";
 // import { defineConfig } from "vite";
@@ -42,13 +22,12 @@
 
 
 
-
-
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
+  base: "/", // Ensure this is set correctly for your deployment
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -62,5 +41,4 @@ export default defineConfig({
     },
   },
 });
-
 
